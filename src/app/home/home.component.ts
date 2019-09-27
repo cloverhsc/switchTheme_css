b887c114d9dd915d5bceea50308abda215da14cc
Clover <clover.hsc@gmail.com>
2019-09-27 15:27:13 +0800

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.component.html',
-	styleUrls: [ './home.component.css' ],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: [ './home.component.css' ],
 })
 export class HomeComponent implements OnInit {
-	public chgTheme = false;
-	constructor() {}
+  public chgTheme = false;
 
-	ngOnInit() {}
+  private el: ElementRef;
+  private themeContainer: HTMLElement;
+  constructor(private element: ElementRef) {
+    this.el = this.element;
+  }
 
-	myFunc() {
-		let styles = getComputedStyle(document.documentElement);
-		let value = String(styles.getPropertyValue('--bg-color')).trim();
-		document.getElementById('template-default').style.setProperty('--tp-background-color', '#d9444a');
-		console.log(value);
-	}
+  ngOnInit() {
+    this.themeContainer = this.el.nativeElement.querySelector('.theme-container');
+  }
 
-	ChangeTheme(changed: boolean) {
-		console.log(changed);
-		console.log(this.chgTheme);
-	}
+  myFunc() {
+    const styles = getComputedStyle(document.documentElement);
+    const value = String(styles.getPropertyValue('--bg-color')).trim();
+    document.getElementById('template-default').style.setProperty('--tp-background-color', '#d9444a');
+    console.log(value);
+  }
+
+  ChangeTheme(changed: boolean) {
+    console.log(changed);
+    console.log(this.chgTheme);
+    if (changed) {
+      this.themeContainer.classList.add('light');
+    } else {
+      this.themeContainer.classList.remove('light');
+    }
+  }
 }
